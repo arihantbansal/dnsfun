091258a05ca0c540fed813d6bf6d494578bd1b16
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2023-03-31 21:14:14 +0530

    My Signed Commit
    Signed-off-by: arihantbansal <17180950+arihantbansal@users.noreply.github.com>

--- src/handler.rs
+++ src/handler.rs
@@ -1,12 +1,73 @@
+use std::sync::{atomic::AtomicU64, Arc};
+
 use crate::Options;
-use trust_dns_server::server::{Request, RequestHandler, ResponseHandler, ResponseInfo};
+use trust_dns_server::{
+    client::rr::LowerName,
+    proto::op::{Header, MessageType, OpCode, ResponseCode},
+    server::{Request, RequestHandler, ResponseHandler, ResponseInfo},
+};
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("Invalid OpCode {0:}")]
+    InvalidOpCode(OpCode),
+    #[error("Invalid MessageType {0:}")]
+    InvalidMessageType(MessageType),
+    #[error("Invalid Zone {0:}")]
+    InvalidZone(LowerName),
+    #[error("IO error: {0:}")]
+    Io(#[from] std::io::Error),
+}
 
 #[derive(Clone, Debug)]
-pub struct Handler {}
+pub struct Handler {
+    pub counter: Arc<AtomicU64>,
+    pub root_zone: LowerName,
+    pub counter_zone: LowerName,
+    pub myip_zone: LowerName,
+    pub hello_zone: LowerName,
+}
 
 impl Handler {
     pub fn from_options(_options: &Options) -> Self {
         Handler {}
+    }
+
+    async fn do_handle_request<R: ResponseHandler>(
+        &self,
+        request: &Request,
+        response: R,
+    ) -> Result<ResponseInfo, Error> {
+        if request.op_code() != OpCode::Query {
+            return Err(Error::InvalidOpCode(request.op_code()));
+        }
+
+        if request.message_type() != MessageType::Query {
+            return Err(Error::InvalidMessageType(request.message_type()));
+        }
+
+        match request.query().name() {
+            name if self.myip_zone.zone_of(name) => {
+                self.do_handle_request_myip(request, response).await
+            }
+            name if self.counter_zone.zone_of(name) => {
+                self.do_handle_request_counter(request, response).await
+            }
+            name if self.hello_zone.zone_of(name) => {
+                self.do_handle_request_hello(request, response).await
+            }
+            name if self.root_zone.zone_of(name) => {
+                self.do_handle_request_default(request, response).await
+            }
+            name => Err(Error::InvalidZone(name.clone())),
+        }
+    }
+
+    async fn do_handle_request_myip<R: ResponseHandler>(
+        &self,
+        request: &Request,
+        mut responder: R,
+    ) -> Result<ResponseInfo, Error> {
     }
 }
 
@@ -14,9 +75,17 @@ impl Handler {
 impl RequestHandler for Handler {
     async fn handle_request<R: ResponseHandler>(
         &self,
-        _request: &Request,
-        _response: R,
+        request: &Request,
+        response: R,
     ) -> ResponseInfo {
-        todo!()
+        match self.do_handle_request(request, response).await {
+            Ok(info) => info,
+            Err(error) => {
+                error!("Error in RequestHandler: {error}");
+                let mut header = Header::new();
+                header.set_response_code(ResponseCode::ServFail);
+                header.into()
+            }
+        }
     }
 }
